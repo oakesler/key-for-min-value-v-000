71e5351089501b67999586a2d58f944485973046
Olivia Kesler <oakesler@protonmail.ch>
2019-07-26 18:45:20 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@ def key_for_min_value(name_hash)
   value_array = []
   key_array = []
   smallest_value = 0
+  final_value = ""
   
   name_hash.each do |key, value|
     key_array << key
@@ -11,26 +12,28 @@ def key_for_min_value(name_hash)
   value_array.combination(2).to_a.each do |section|
     if smallest_value == 0 
       if section[0] < section[1]
-        section[0] == smallest_value
+        smallest_value += section[0]
         elsif section[0] > section[1]
-        section[1] == smallest_value
+        smallest_value += section[1]
     if smallest_value > 0 
       if section[0] < smallest_value
-        section[0] == smallest_value
+        smallest_value -= smallest_value
+        smallest_value += section[0]
         elsif section[1] < smallest_value
-        section[1] == smallest_value
+        smallest_value -= smallest_value
+        smallest_value += section[0]
         else
-          smallest_value += 0
+          smallest_value + 0
         end
       end
     end
-    smallest_value
   end
 
-  #key_array.map do |index, value|
-    #if "#{value}" == "#{smallest_value}"
-      #key_array[index]
-      #end
-    #end
+  key_array.each  do |index, value|
+    if "#{value}" == "#{smallest_value}"
+      final_value == key_array[index]
+      end
+    end
   end
+  final_value
 end
