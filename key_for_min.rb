5aaf02b6fcd3f5bef3996dbd45cee664e6dcf28d
Olivia Kesler <oakesler@protonmail.ch>
2019-07-26 22:45:11 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,29 +2,27 @@ require "pry"
 def key_for_min_value(name_hash)
   value_array = []
   key_array = []
-  smallest_value = 0
   final_value = ""
+  smallest_value = 0
   
   name_hash.each do |key, value|
     key_array << key
     value_array << value 
-    smallest_value == value_array[0]
-    #binding.pry
-    end
+  end
   
   value_array.combination(2).to_a.each do |section|
-    if section[0] < smallest_value
+    binding.pry
+    if section[0] <= value_array[0]
       smallest_value == 0
-      smallest_value == section[0]
-      elsif section[1] < smallest_value
+      smallest_value += section[0]
+    elsif section[1] <= value_array[0]
       smallest_value == 0
-        smallest_value == section[1]
-        binding.pry
+      smallest_value += section[1]
       end
+    #else
     end
 
   key_array.each do |key|
-    #binding.pry
     if name_hash[key] == smallest_value 
       final_value == "#{key}"
     end
