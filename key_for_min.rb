84bc435f215870bc48b53aa3373ebbe5cad7a3c2
Olivia Kesler <oakesler@protonmail.ch>
2019-07-26 22:28:20 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,18 +8,23 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     key_array << key
     value_array << value 
-    smallest_value += value_array[0]
+    smallest_value == value_array[0]
+    #binding.pry
     end
   
   value_array.combination(2).to_a.each do |section|
     if section[0] < smallest_value
+      smallest_value == 0
       smallest_value == section[0]
       elsif section[1] < smallest_value
+      smallest_value == 0
         smallest_value == section[1]
+        binding.pry
       end
     end
 
   key_array.each do |key|
+    #binding.pry
     if name_hash[key] == smallest_value 
       final_value == "#{key}"
     end
