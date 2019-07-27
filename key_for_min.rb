9e1711c45fc4cdbf08762b08f45a4de91112f1fc
Olivia Kesler <oakesler@protonmail.ch>
2019-07-27 04:26:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,18 +13,33 @@ def key_for_min_value(name_hash)
   
   value_array.each do |value|
     if value < value_array[0]
+<<<<<<< HEAD
       smallest_value -= smallest_value
       smallest_value += value
     elsif value > value_array[0]
     smallest_value -= smallest_value
     smallest_value += value_array[0]
+=======
+      smallest_value == value
+    else smallest_value + 0 
+>>>>>>> 4749a166f436509a7d3b7ed5fc492e7a48783d3e
   end
 end
 
   key_array.each do |key|
+<<<<<<< HEAD
     if name_hash[key] == smallest_value
       new_array.push(key)
     end
   end
   new_array[0]
+=======
+    #binding.pry
+    if name_hash[key] == smallest_value 
+      "#{key}"
+    else
+      "blah"
+    end
+  end
+>>>>>>> 4749a166f436509a7d3b7ed5fc492e7a48783d3e
 end
